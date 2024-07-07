d67714d319d1a764c765c3b8a418e85b01fc8b09
Chukwuemeka David <159067957+chuksOfKrownpay@users.noreply.github.com>
2024-07-07 02:47:44 +0100

    add the features section responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Expect from "./components/Expect"
+import Features from './components/Features';
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Expect />
+      <Features />
     </div>
   );
 };
