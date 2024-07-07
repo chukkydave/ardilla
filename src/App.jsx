f8a21c2b0d868da691ae3cac058b1495485a64cc
Chukwuemeka David <159067957+chuksOfKrownpay@users.noreply.github.com>
2024-07-07 03:20:01 +0100

    add responsive invest section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Expect from "./components/Expect"
 import Features from './components/Features';
+import Invest from './components/Invest';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <Expect />
       <Features />
+      <Invest />
     </div>
   );
 };
